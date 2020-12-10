c436dd37197abad76aaf31e35afa87186b8eb780
Khikuroba <anhtd98@gmail.com>
2020-12-10 15:53:22 +0700

    lession 21

--- routes/user.route.js
+++ routes/user.route.js
@@ -13,13 +13,13 @@ router.get('/', authMiddleware.requireAuth, controller.index);
 router.get('/cookie', (req, res , next) => {
     res.cookie('user-id', 12345);
     res.send('Hello')
-})
+});
 
 router.get('/create', controller.create);
 
-router.get('/:id', controller.get)
+router.get('/:id', controller.get);
 
-router.get('/search', controller.search)
+router.get('/search', controller.search);
 
 router.post('/create',upload.single('avatar'), validate.postCreate, controller.postCreate);
 
